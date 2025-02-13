49160f52763d6d65524116ea863ea7de75a5ccaa
Ace360Degree-CC <your-email@example.com>
2025-02-13 18:20:45 +0530

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>JC Ledger</title>
+
+        
+
+        
+    </head>
+    <body class="container">
+        <h1>Welcome to JC Venture</h1>
+    </body>
+</html>
